b51ed1629033be06ee8400351c34b9a10ce59d8b
Robert Lillack <rob@lillack.net>
2025-10-05 13:27:09 +0200

    Add Fish

---
+++ config/fish/config.fish
@@ -0,0 +1,48 @@
+function fish_user_key_bindings
+    bind \e\[rA\;BS~ backward-kill-token
+    bind \e\[rA\;DEL~ kill-token
+    bind \e\[rA\;LEFT~ prevd-or-backward-token
+    bind \e\[rA\;RIGHT~ nextd-or-forward-token
+
+    bind \e\[rC\;BS~ backward-kill-word
+    bind \e\[rC\;DEL~ kill-word
+    bind \e\[rC\;LEFT~ backward-word
+    bind \e\[rC\;RIGHT~ forward-word
+end
+
+function fish_prompt -d "Write out the prompt"
+    set -l suffix '$'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        set suffix '#'
+    end
+    echo -n -s $USER '@' (prompt_hostname) ':' (prompt_pwd) (fish_vcs_prompt) $suffix ' '
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g fish_autosuggestion_enabled 0
+    set -g fish_greeting ''
+
+    set -g fish_complete_path $fish_complete_path /opt/homebrew/share/fish/vendor_completions.d
+    fish_config theme choose Tomorrow
+
+    if which birthdays &>/dev/null
+        birthdays
+    end
+end
+
+if which hx &>/dev/null
+    set -gx EDITOR hx
+else if which vim &>/dev/null
+    set -gx EDITOR vim
+else
+    set -gx EDITOR vi
+end
+set -gx VISUAL $EDITOR
+
+if test -f /opt/homebrew/bin/brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    fish_add_path /opt/homebrew/bin
+end
+
+fish_add_path ~/bin ~/{go,.cargo,.yarn,.local}/bin >/dev/null
